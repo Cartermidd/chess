596741b02abfe9ac9509d9161752a92025801796
Carter Middleton <carterwmiddleton@gmail.com>
2026-03-26 16:47:23 -0600

    All unit tests updated

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,9 +1,8 @@
 package client;
 
+import chess.ChessGame;
 import org.junit.jupiter.api.*;
-import requests.AuthorizedRequest;
-import requests.LoginRequest;
-import requests.RegisterRequest;
+import requests.*;
 import server.Server;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -82,10 +81,60 @@ public class ServerFacadeTests {
     @Test
     void logoutFail() throws Exception{
         assertThrows(Exception.class, () -> {
-            var result = facade.register(new RegisterRequest(new String[]{"player1", "password", "p1@email.com"}));
-            facade.logout(new AuthorizedRequest(result.getAuthToken()));
-            facade.login(new LoginRequest(new String[]{"player1", "badpassword"}));
+            facade.logout(new AuthorizedRequest("whoops"));
         });
     }
+
+    @Test
+    void listGames() throws Exception {
+        var result = facade.register(new RegisterRequest(new String[]{"player1", "password", "p1@email.com"}));
+        var listGamesResult = facade.listGames(new AuthorizedRequest(result.getAuthToken()));
+        assertNotNull(listGamesResult);
+    }
+
+    @Test
+    void listGamesFail() throws Exception{
+        assertThrows(Exception.class, () -> {
+            facade.listGames(new AuthorizedRequest("whoops"));
+        });
+    }
+
+
+    @Test
+    void createGame() throws Exception {
+        var result = facade.register(new RegisterRequest(new String[]{"player1", "password", "p1@email.com"}));
+        var createGameResult = facade.createGame(new CreateGameRequest("gameName"));
+        assertNotNull(createGameResult.getGameID());
+    }
+
+    @Test
+    void createGameFail() throws Exception{
+        assertThrows(Exception.class, () -> {
+            facade.createGame(new CreateGameRequest("gameName"));
+        });
+    }
+
+    @Test
+    void joinGame() throws Exception {
+        assertDoesNotThrow(() ->{
+            var result = facade.register(new RegisterRequest(new String[]{"player1", "password", "p1@email.com"}));
+            var createGameResult = facade.createGame(new CreateGameRequest("gameName"));
+            facade.joinGame(new JoinGameRequest(ChessGame.TeamColor.BLACK, 1));
+            facade.joinGame(new JoinGameRequest(ChessGame.TeamColor.WHITE, 1));
+        }
+        );
+    }
+
+    @Test
+    void joinGameFail() throws Exception{
+        assertThrows(Exception.class,() ->{
+                    var result = facade.register(new RegisterRequest(new String[]{"player1", "password", "p1@email.com"}));
+                    var createGameResult = facade.createGame(new CreateGameRequest("gameName"));
+                    facade.joinGame(new JoinGameRequest(ChessGame.TeamColor.BLACK, 1));
+                    facade.joinGame(new JoinGameRequest(ChessGame.TeamColor.BLACK, 1));
+                }
+        );
+    }
+
 
 }
